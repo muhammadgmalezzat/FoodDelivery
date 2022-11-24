23bfa1967fdaf001baad20bce88950a476905db6
muhammad gmal ezzat <59686905+muhammadgmalezzat@users.noreply.github.com>
2022-11-24 21:01:04 +0200

    create app

---
+++ food-delivery/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      {/* header */}
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
